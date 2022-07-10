24cbf5e0f7782d4d9cbfc55691ca1d9972874813
0dminnimda <0dminnimda@gmail.com>
2022-07-10 23:14:32 +0300

    pygame_example.py: make easier to read

--- pygame_example.py
+++ pygame_example.py
@@ -3,12 +3,15 @@ from sortvis import *
 
 length = 2**10
 array = list(range(1, length + 1))
+# randomize the array
 random.shuffle(array)
+# or reverse the array
 # array = array[::-1]
 
 # sort = BubbleSort
 sort = CocktailShakerSort
 # sort = MergeSort
+
 visualizer = SortVisualizer()
 # visualizer.framerate = 60
-visualizer.run(sort, array, 30)
+visualizer.run(sort, array, ops_per_frame=30)
